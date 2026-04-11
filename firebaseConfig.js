2d6b514d32463681b5f7658723e5dc1024d91da5
Zara K <zarakeyani@gmail.com>
2026-04-11 21:02:46 +0100

    Added database configuration file

---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNZI4Qm8cpoG1yeX1-tAEN2eG5ft_Yjno",
+  authDomain: "agile-project-25d43.firebaseapp.com",
+  projectId: "agile-project-25d43",
+  storageBucket: "agile-project-25d43.firebasestorage.app",
+  messagingSenderId: "21346969728",
+  appId: "1:21346969728:web:6527c0da74c41962875e65"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+// make db and auth globally accessible
+window.db = db;
+window.auth = auth;
+
+// To use firebase just import <script type="module" src="firebaseConfig.js"></script> in your html page header :)
