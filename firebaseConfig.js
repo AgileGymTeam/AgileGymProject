65a073dd3627cf5764ebf2551c60ae03b002fb78
Zara K <zarakeyani@gmail.com>
2026-04-25 20:06:03 +0100

    member class view functionality

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   projectId: "agile-project-25d43",
   storageBucket: "agile-project-25d43.firebasestorage.app",
   messagingSenderId: "21346969728",
-  appId: "1:21346969728:web:6527c0da74c41962875e65"
+  appId: "1:21346969728:web:6527c0da74c41962875e65",
+  databaseURL: "https://agile-project-25d43-default-rtdb.europe-west1.firebasedatabase.app",
 };
 
 // Initialize Firebase
@@ -17,8 +18,5 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
-// make db and auth globally accessible
-window.db = db;
-window.auth = auth;
-
-// To use firebase just import <script type="module" src="firebaseConfig.js"></script> in your html page header :)
+// export
+export { db, auth };
